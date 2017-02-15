cb22edfa77f5b53bb73e722cc83eb128bad6434d
Karthick Perumal <Karamya@users.noreply.github.com>
2017-02-15 16:18:48 +0100

    Add files via upload

---
+++ combine_two_parts.py
@@ -0,0 +1,8 @@
+import pandas
+from data_cleaning import df2
+from clean_team_data import X_team
+
+print("Number of columns in the first part is ", len(df2.columns))
+print("Number of columns in the second part is ", len(X_team.columns))
+clean_data = df2.join(X_team)
+print("Number of columns in the complete & cleaned data sets  is ", len(clean_data.columns))
