139d8948459eed12f273a170688e8fa99dcaa1e7
Mark Hashimoto <mark.m.hashimoto@gmail.com>
2017-02-15 17:53:52 +0100

    test

--- combine_two_parts.py
+++ combine_two_parts.py
@@ -5,4 +5,6 @@ from clean_team_data import X_team
 print("Number of columns in the first part is ", len(df2.columns))
 print("Number of columns in the second part is ", len(X_team.columns))
 clean_data = df2.join(X_team)
+y = clean_data["Grant.Status"]
+X = clean_data.drop(["Grant.Status"], axis=1)
 print("Number of columns in the complete & cleaned data sets  is ", len(clean_data.columns))
